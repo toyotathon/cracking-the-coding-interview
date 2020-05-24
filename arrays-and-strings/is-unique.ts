e1c3eb3f0e9d76159c22dccde48035caaf557cb3
Jason Valenzuela <jason.e.valenzuela@gmail.com>
2020-05-24 14:46:37 -0700

    adding vscode settings

--- arrays-and-strings/is-unique.ts
+++ arrays-and-strings/is-unique.ts
@@ -1,16 +1,18 @@
 import { readFileStr } from "https://deno.land/std/fs/read_file_str.ts";
 
-const checkUniqueCharactersNoStructure = () => {
-  // TODO: implement the same algorithm without using data structures.
-};
+/**
+ * Helper function that removes all whitespace from string and parses to array
+ * @param current 
+ */
+const cleanString = (current: string): string[] =>
+  current.replace(/\s/g, "").split("");
 
 /**
  * Given a string, check that all of the characters in the string are unique.
  * @param current
  */
 const checkUniqueCharacters = (current: string): boolean => {
-  const cleanedString = current.replace(/\s/g, "").split("");
-  const characterMap = cleanedString.reduce((charToCount, curr) => {
+  const characterMap = cleanString(current).reduce((charToCount, curr) => {
     const currentCount = charToCount.get(curr);
     if (currentCount !== undefined) {
       return charToCount.set(curr, currentCount + 1);
@@ -18,6 +20,14 @@ const checkUniqueCharacters = (current: string): boolean => {
     return charToCount.set(curr, 1);
   }, new Map<string, number>());
   return Array.from(characterMap.values()).every(count => count === 1);
+};
+
+/**
+ * Same function as `checkUniqueCharacters`, but does not use any data structures.
+ * @param current
+ */
+const checkUniqueCharactersNoStructure = (current: string): boolean => {
+  return true;
 };
 
 const sherlockFile: string = await readFileStr("./sample-data/sherlock.txt");
